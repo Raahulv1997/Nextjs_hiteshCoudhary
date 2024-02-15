de86a28241572a4fd3149ee5dd3a64c56d6ab429
Rahuldatapure <rahul.verma@datapure.co>
2024-02-15 15:32:16 +0530

    commit by rahuldatapure

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -16,7 +16,7 @@ const Login = () => {
   }
   return (
     <div className='flex flex-col items-center justify-center min-h-screen py-2'>
-      <h1>Login</h1>
+      <h1>Loginn</h1>
      
 <hr/>
       <hr/>
